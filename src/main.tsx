41f753636430f13f779ae58b54aefb895fdc7ca0
G7-dev <1255233299@qq.com>
2026-03-25 11:10:55 +0800

    chore: force rebuild with new hash

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./app/App.tsx";
 import "./styles/index.css";
 
+// Build version: 2025-03-25-10-58-00
 createRoot(document.getElementById("root")!).render(<App />);
+
